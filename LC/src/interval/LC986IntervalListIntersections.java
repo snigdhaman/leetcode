d32d5d04487263eb0d13e3d98d27c8ba028819e5
s0c0vlo <snigdhaman.chatterje@walmart.com>
2025-06-21 19:32:11 +0530

    LC986 Interval List Intersection medium

---
+++ LC/src/interval/LC986IntervalListIntersections.java
@@ -0,0 +1,48 @@
+package interval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC986IntervalListIntersections {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        if (firstList.length == 0 || secondList.length == 0) {
+            return new int[0][0];
+        }
+        List<int[]> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < firstList.length && j < secondList.length) {
+            int start1 = firstList[i][0];
+            int end1 = firstList[i][1];
+            int start2 = secondList[j][0];
+            int end2 = secondList[j][1];
+
+            //No intersection
+            //If 1st interval starts after 2nd ends, increment j
+            if (start1 > end2) {
+                j++;
+            }
+            //No intersection
+            //If 2nd interval starts after 1st ends, increment i
+            else if (start2 > end1) {
+                i++;
+            }
+            else {
+                list.add(new int[]{Math.max(start1, start2), Math.min(end1, end2)});
+                if (end1 > end2) {
+                    j++;
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        int[][] res = new int[list.size()][2];
+        int idx = 0;
+        for (int[] el : list) {
+            // System.out.println(el[0] + "::" + el[1]);
+            res[idx] = el;
+            idx++;
+        }
+        return res;
+    }
+}
